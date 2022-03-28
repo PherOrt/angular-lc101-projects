77431e41c797b0a220a4dabf5b27c3e5958e1d4f
COrtega <christopheraortega@gmail.com>
2022-03-28 10:34:48 -0500

    update

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,34 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+
+  confirmTakeoff(){
+    if(window.confirm('Confirm that the shuttle is ready for takeoff.')){
+       //document.getElementById('status').innerHTML = 'Shuttle in flight.';
+      
+       this.message = "Shuttle in flight.";
+       this.height = 10000;
+       document.getElementById('background-color').style.backgroundColor = "blue";
+    }
+ }
+ landingAlert(){
+   window.alert('The shuttle is landing. Landing gear engaged.');
+   this.message = "The shuttle has landed.";
+   document.getElementById('background-color').style.backgroundColor = "green";
+   this.height = 0;
+ }
+ abortMission(){
+   if(window.confirm("Do you really want to abort the mission?")){
+     this.message = 'Mission aborted.';
+     document.getElementById('background-color').style.backgroundColor = "red";
+     this.height = 0;
+   }
+ }
+ shipMove(){
+   document.getElementById("up").addEventListener("click",function(){
+    document.getElementById("rocketImg").height = height + 10;
+   })
+
+   
+ }
 }
