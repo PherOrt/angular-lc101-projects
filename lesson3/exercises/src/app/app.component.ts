fe809aee4c20f645ecba218e49bd9a8f6fc41140
COrtega <christopheraortega@gmail.com>
2022-03-28 11:03:29 -0500

    update

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,34 +12,69 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled = true;
 
-  confirmTakeoff(){
-    if(window.confirm('Confirm that the shuttle is ready for takeoff.')){
-       //document.getElementById('status').innerHTML = 'Shuttle in flight.';
-      
-       this.message = "Shuttle in flight.";
-       this.height = 10000;
-       document.getElementById('background-color').style.backgroundColor = "blue";
+  gutterCheck(width, height) {
+    if (width > 260000 || width < 0 || height < 0 || height > 340000) {
+      this.color = 'orange';
+    } else {
+      this.color = 'blue';
     }
- }
- landingAlert(){
-   window.alert('The shuttle is landing. Landing gear engaged.');
-   this.message = "The shuttle has landed.";
-   document.getElementById('background-color').style.backgroundColor = "green";
-   this.height = 0;
- }
- abortMission(){
-   if(window.confirm("Do you really want to abort the mission?")){
-     this.message = 'Mission aborted.';
-     document.getElementById('background-color').style.backgroundColor = "red";
-     this.height = 0;
-   }
- }
- shipMove(){
-   document.getElementById("up").addEventListener("click",function(){
-    document.getElementById("rocketImg").height = height + 10;
-   })
+  }
 
-   
- }
+  moveRocket(rocketImage, direction) {
+    if (direction === 'right') {
+      let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+    } else if (direction === 'left') {
+      let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width= this.width - 10000;
+    } else if (direction === 'down') {
+      let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height - 10000;
+    } else {
+      let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height + 10000;
+    }
+    this.gutterCheck(this.width, this.height);
+  }
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+      this.color = 'blue';
+      this.height = 10000;
+      this.width = 0;
+      this.message = 'Shuttle in flight.';
+      this.takeOffEnabled = false;
+    }
+  }
+
+  handleLand(rocketImage) {
+    window.alert('The shuttle is landing. Landing gear engaged.');
+    this.color = 'green';
+    this.height = 0;
+    this.width = 0;
+    this.message = 'Shuttle landed.';
+    this.takeOffEnabled = true;
+    rocketImage.style.bottom = '0px';
+    rocketImage.style.left = '0px';
+  }
+
+  handleMissionAbort(rocketImage) {
+    let result = window.confirm('Are you sure you want to end the mission?');
+    if (result) {
+      this.color = 'red';
+      this.height = 0;
+      this.width = 0;
+      this.message = 'Mission aborted.';
+      this.takeOffEnabled = true;
+      rocketImage.style.bottom = '0px';
+      rocketImage.style.left = '0px';
+    }
+  }
 }
